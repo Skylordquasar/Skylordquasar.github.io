ef42e7706a3fddba61213617bc1a7a99977c96c6
Skylordquasar <31858625+Skylordquasar@users.noreply.github.com>
2017-10-23 08:16:42 -0700

    confirming uniformity

---
+++ magicscript.js
@@ -0,0 +1,8 @@
+function shakemagic(){
+	
+	//grab image by id
+	document.getElementById("magicball").src= "r/magicgoogle8ball1.jpg";
+	
+	//grab the results
+	document.getElementById("result").innerHTML="Just google it!";
+}
